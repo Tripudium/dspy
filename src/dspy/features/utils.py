bcf7098a87bb9e4281ea9d9861f3b9c37b3e628b
Martin Lotz <lotz@devel002.htz.trnk.lan>
2025-03-16 09:31:56 +0000

    Added signal pnl funtions including signals and syncing with trades

--- src/dspy/features/utils.py
+++ src/dspy/features/utils.py
@@ -20,17 +20,3 @@ def get_products(df: pl.DataFrame, cols: list[str]) -> list[str]:
     else:
         products = []
     return products
-
-def sync_with_book(
-        df: pl.DataFrame, 
-        bdf: pl.DataFrame, 
-        on: str = "ts", 
-        cols: list[str] = ['prc_s0', 'prc_s1', 'vol_s0', 'vol_s1', 'sig_pnl']
-        ) -> pl.DataFrame:
-    """
-    Sync a dataframe with the book dataframe on a given timestamp column.
-    """
-    bdf = bdf.select([on, *cols])
-    
-    df = df.join_asof(bdf, on=on, strategy="backward")
-    return df
